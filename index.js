ca0ce5fde8318c6c8666636f6fcfe3ed47c02b28
tabbyrhett <tabbyrhett@mail.ru>
2022-02-27 00:27:38 +0300

    hbs

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
 
 const PORT = process.env.PORT || 5000
 
-var app = express()
+var app = express() 
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
 })
 
-app.engine('hbseng', hbs.engine)
-app.set('view engine', 'hbseng') // using hadlebars
-app.set('views', 'views') // folder with views names views lol
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs') // using hadlebars
+app.set('views', 'views') // folder with views named views lol
+
+app.use(todoRoutes)
 
 async function start() {
     try {
@@ -33,8 +36,8 @@ async function start() {
 
 start()
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'public/index.html')
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + 'public/index.html')
+// })
