31582917a75f5df1aa1d3adc1e4ffc47ced514fd
tabbyrhett <tabbyrhett@mail.ru>
2022-02-26 22:43:07 +0300

    bars

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-var express = require('express')
+const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+
+const PORT = process.env.PORT || 5000
 
 var app = express()
-var server = app.listen(process.env.PORT || 5000)
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbseng', hbs.engine)
+app.set('view engine', 'hbseng') // using hadlebars
+app.set('views', 'views') // folder with views names views lol
+
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://tabby:flowless@cluster0.8qffk.mongodb.net/todos?retryWrites=true&w=majority', {
+
+        })
+        app.listen(PORT, () => {
+            console.log('LSNING ON PORT ' + PORT)
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+// var server = app.listen(PORT, () => {
+//     console.log('LSNING ON PORT ' + PORT)
+// })
+
+start()
 
 app.use(express.static('public'))
 
